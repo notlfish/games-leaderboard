2d9e4b5ac465b07b0aa2cd84b8b5dfe5491ff0b4
Lucas Ferrari Soto <lucasmfsoto@gmail.com>
2021-08-31 17:36:16 -0300

    Add HTML and basic styling

--- src/index.js
+++ src/index.js
@@ -1,11 +1,1 @@
 import './style.css';
-
-function component() {
-  const element = document.getElementsByTagName('h1')[0];
-
-  element.innerHTML = 'Project setup';
-
-  return element;
-}
-
-document.body.appendChild(component());
